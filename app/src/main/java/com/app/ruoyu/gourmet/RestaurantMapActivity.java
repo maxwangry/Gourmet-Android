a6b2abf3d4c13df35682e8a70b0c26b80e3a5326
dandanbiu <dandanbiubiu@gmail.com>
2017-01-12 22:41:17 -0800

    Add a new activity - Restaurant Map

---
+++ app/src/main/java/com/app/ruoyu/gourmet/RestaurantMapActivity.java
@@ -0,0 +1,13 @@
+package com.app.ruoyu.gourmet;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class RestaurantMapActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_map);
+    }
+}
