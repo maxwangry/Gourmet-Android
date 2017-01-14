26efc79b8e6703f293157f77ae6a5921dc69a49e
dandanbiu <dandanbiubiu@gmail.com>
2017-01-13 21:48:23 -0800

    Add a marker to the map, Marker marks a position in the map with corresponding latitude and longitude.

--- app/src/main/java/com/app/ruoyu/gourmet/RestaurantMapActivity.java
+++ app/src/main/java/com/app/ruoyu/gourmet/RestaurantMapActivity.java
@@ -3,11 +3,27 @@ package com.app.ruoyu.gourmet;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-public class RestaurantMapActivity extends FragmentActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class RestaurantMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_map);
+        MapFragment mapFragment =
+                (MapFragment) getFragmentManager().findFragmentById(R.id.restaurant_map);
+
+        // This function automatically initializes the maps system and the view.
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 }
