dba8c326adb33ec204c7e1de9acabf4d20f7dd5f
dandanbiu <dandanbiubiu@gmail.com>
2017-01-17 19:39:26 -0800

    Added Espresso Test

---
+++ app/src/main/java/com/app/ruoyu/gourmet/MainActivity.java
@@ -0,0 +1,35 @@
+package com.app.ruoyu.gourmet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonYelp;
+    Button buttonBackend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonYelp = (Button) findViewById(R.id.button_yelp);
+        buttonBackend = (Button) findViewById(R.id.button_backend);
+
+        buttonYelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonBackend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonBackend.setText("New text");
+            }
+        });
+    }
+}
