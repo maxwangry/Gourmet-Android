8aaf13f73a806fe310d7a7acc4d99c89562d9f28
dandanbiu <dandanbiubiu@gmail.com>
2017-01-17 20:15:00 -0800

    Added Login UI

--- app/src/main/java/com/app/ruoyu/gourmet/MainActivity.java
+++ app/src/main/java/com/app/ruoyu/gourmet/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 buttonBackend.setText("New text");
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
